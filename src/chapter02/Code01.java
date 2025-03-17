17203c2ad79ea7050465bea45c307adffaa578c0
hanmose <nightsound5124@gmail.com>
2025-03-17 11:52:22 +0900

    main Method 사용법 , 연결 연산자 , 덧셈 연산자 , println , print 차이점

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class Code01 {
+    public static void main(String[] args){
+        System.out.println("AISoftware");
+        System.out.println("Mose Han");
+        System.out.printf("Grade 1\n");
+        System.out.println("result:"+(100+200));
+        System.out.println(100 + 200 + "won"); //300won
+    }
+}
