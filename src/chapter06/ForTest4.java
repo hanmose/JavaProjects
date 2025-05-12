40d3735e697529c41413879129532656d8722968
hanmose <nightsound5124@gmail.com>
2025-05-12 10:21:26 +0900

    6장 반복문-For3-4, 경우의 수, 합계

---
+++ src/chapter06/ForTest4.java
@@ -0,0 +1,15 @@
+package chapter06;
+
+public class ForTest4 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 10;
+
+        for (int i = 1;i <=n;i++)
+        {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
