dfbcd1936f8d594309e832ba7d274acbc09e4f2c
hanmose <nightsound5124@gmail.com>
2025-05-12 09:41:14 +0900

    6장 반복문-For1, Enhanced For(for-each)

---
+++ src/chapter06/ForTest.java
@@ -0,0 +1,20 @@
+package chapter06;
+
+import java.util.ArrayList;
+
+public class ForTest {
+    public static void main(String[] args)
+    {
+        String[] names = { "동해","물과","백두","산이","마르"};
+
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + ": 서울정수캠퍼스 인공지능소프트웨어과");
+        }
+        ArrayList<String> arrayList = new ArrayList<>();
+
+
+        String name = "";
+        for
+    }
+}
