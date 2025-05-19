72cd138949238e63cb1c5a40048c460abf714ca2
hanmose <nightsound5124@gmail.com>
2025-05-19 11:48:26 +0900

    7장 클래스와 객체

---
+++ src/chapter07/Human.java
@@ -0,0 +1,16 @@
+package chapter07;
+
+public class Human {
+
+        String name;
+        int age;
+
+        public void walk(String place){
+            System.out.println(name+"가 "+ place+"까지 걷는가");
+        }
+
+        public void eat(String food){
+            System.out.println(food +"을(를) 먹는다.");
+        }
+    }
+
