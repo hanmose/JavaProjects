ea8168b763ce5a2580ecf3b7a5f554990e358e54
hanmose <nightsound5124@gmail.com>
2025-06-02 11:50:20 +0900

    7장 클래스와 객체 LAB 뜀뛰기하는 토끼

---
+++ src/chapter07/rabbit/rabbitTest2.java
@@ -0,0 +1,10 @@
+package chapter07.rabbit;
+
+public class rabbitTest2 {
+    public static void main(String[] args) {
+        Rabbit rabb = new Rabbit();
+        System.out.println(rabb);
+        Rabbit rabb2 = new Rabbit("빨강코 토끼",70,30);
+        System.out.println(rabb2);
+    }
+}
